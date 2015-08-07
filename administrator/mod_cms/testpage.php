1eae0ba8d55ebbd154c870cba2f4ea197bae36af
noppolgap <noppol.vong@gmail.com>
2015-08-07 13:18:21 +0700

    Change Config for DEV

---
+++ administrator/mod_cms/testpage.php
@@ -0,0 +1,30 @@
+<?php
+require("../../assets/configs/config.inc.php");
+require("../../assets/configs/connectdb.inc.php");
+require("../../assets/configs/function.inc.php");
+?>
+<!doctype html>
+<html>
+<head>
+<? require('../inc_meta.php'); ?>		
+</head>
+
+<body>
+<? require('../inc_header.php'); ?>		
+<div class="main-container">
+	<div class="main-body marginC">
+		<?php require('../inc_side.php'); ?>
+		 <?php require('userMain.php'); ?>
+
+		<div class="clear"></div>	
+	</div>
+</div>	
+<? require('../inc_footer.php'); ?>		
+<link rel="stylesheet" type="text/css" href="../../assets/font/ThaiSans-Neue/font.css" media="all" >
+<link rel="stylesheet" type="text/css" href="../master/style.css" media="all" />
+<link rel="stylesheet" type="text/css" href="mod_cms.css" media="all" />
+<script type="text/javascript" src="../master/script.js"></script>		
+<script type="text/javascript" src="mod_cms.js"></script>	
+<?php logs_access('admin','hello'); ?>	
+</body>
+</html>
--- assets/configs/config.inc.php
+++ assets/configs/config.inc.php
@@ -1,25 +1,31 @@
 <?php 
 	session_start();
-	define(_DATA_BASE_HOST_,'localhost');
-	define(_DATA_BASE_NAME_,'campaignac_mu');
-	/*------------ Online ------------*/
-	define(_DATA_BASE_USER_,'campaignac_mu');
-	define(_DATA_BASE_PASS_,'2VIE2bv8');
-	/*------------ Offline ------------
-	define(_DATA_BASE_USER_,'root');
-	define(_DATA_BASE_PASS_,'1234');
+	
+
+	define('_DATA_BASE_HOST_','localhost');
+	
+
+	/*------------ Online ------------
+	define('_DATA_BASE_NAME_','campaignac_mu');
+	define('_DATA_BASE_USER_','campaignac_mu');
+	define('_DATA_BASE_PASS_','2VIE2bv8');*/
+	/*------------ Offline ------------ */
+	define('_DATA_BASE_NAME_','siam_museum');
+	define('_DATA_BASE_USER_','root');
+	define('_DATA_BASE_PASS_','1234');
 	/*------------ Page Config ------------*/
-	define(_TITTLE_SITE_,'สถาบันพิพิธฑ์การเรียนรู้แห่งชาติ (สพร.)');
-	define(_FULL_SITE_PATH_,'http://www.campaignactivity.com/museum');
-	define(_LOG_PATH_,'../../log');
-	define(_LANGUAGE_SITE_,'thai');
+	define('_TITTLE_SITE_','สถาบันพิพิธฑ์การเรียนรู้แห่งชาติ (สพร.)');
+	define('_FULL_SITE_PATH_','http://localhost:8080/SiamMusuem/museumsiam');
+	//define('_FULL_SITE_PATH_','http://www.campaignactivity.com/museum');
+	define('_LOG_PATH_','../../log');
+	define('_LANGUAGE_SITE_','thai');
 	/*------------ E-Mail ------------*/
-	define(_MAIL_HOST_,'mail.campaignactivity.com');
-	define(_MAIL_PORT_,'25');
-	define(_MAIL_USER_,'museum@campaignactivity.com');
-	define(_MAIL_PASS_,'A3coVotO');
+	define('_MAIL_HOST_','mail.campaignactivity.com');
+	define('_MAIL_PORT_','25');
+	define('_MAIL_USER_','museum@campaignactivity.com');
+	define('_MAIL_PASS_','A3coVotO');
 	/*------------ SEO ------------*/	
-	define(_META_KEYWORDS_,'มิวเซียม สยาม, สพร., สถาบันพิพิธภัณฑ์การเรียนรู้แห่งชาติ, พิพิธภัณฑ์, พิพิธภัณฑ์ท้องถิ่น, ศูนย์วัฒนธรรม, แหล่งเรียนรู้, ภูมิปัญญา, มรดกวัฒนธรรม, ประวัติศาสตร์ท้องถิ่น, จดหมายเหตุ, พิพิธภัณฑ์วิทยา, Museum Siam, NDMI, National Discovery Museum Institute, Thai Museums , local museum, museum database, thai museums, cultural center, heritage, cultural heritage, intangible cultural heritage, digital archive, museology');
-	define(_META_DESCRIPTION_,'มิวเซียมสยาม พิพิธภัณฑ์การเรียนรู้ (Museum Siam: Discovery Museum) ภายใต้ สถาบันพิพิธภัณฑ์การเรียนรู้แห่งชาติ (สพร.) เป็นพิพิธภัณฑ์การเรียนรู้แห่งแรกที่เน้น การสร้างประสบการณ์สดใหม่ในการชมพิพิธภัณฑ์ ซึ่งจัดตั้งขึ้นเพื่อเป็นต้นแบบของ แหล่งเรียนรู้ที่น่ารื่นรมย์ และช่วยยกระดับมาตรฐานการจัดการเรียนรู้ในรูปแบบใหม่');
+	define('_META_KEYWORDS_','มิวเซียม สยาม, สพร., สถาบันพิพิธภัณฑ์การเรียนรู้แห่งชาติ, พิพิธภัณฑ์, พิพิธภัณฑ์ท้องถิ่น, ศูนย์วัฒนธรรม, แหล่งเรียนรู้, ภูมิปัญญา, มรดกวัฒนธรรม, ประวัติศาสตร์ท้องถิ่น, จดหมายเหตุ, พิพิธภัณฑ์วิทยา, Museum Siam, NDMI, National Discovery Museum Institute, Thai Museums , local museum, museum database, thai museums, cultural center, heritage, cultural heritage, intangible cultural heritage, digital archive, museology');
+	define('_META_DESCRIPTION_','มิวเซียมสยาม พิพิธภัณฑ์การเรียนรู้ (Museum Siam: Discovery Museum) ภายใต้ สถาบันพิพิธภัณฑ์การเรียนรู้แห่งชาติ (สพร.) เป็นพิพิธภัณฑ์การเรียนรู้แห่งแรกที่เน้น การสร้างประสบการณ์สดใหม่ในการชมพิพิธภัณฑ์ ซึ่งจัดตั้งขึ้นเพื่อเป็นต้นแบบของ แหล่งเรียนรู้ที่น่ารื่นรมย์ และช่วยยกระดับมาตรฐานการจัดการเรียนรู้ในรูปแบบใหม่');
 ?>
 
