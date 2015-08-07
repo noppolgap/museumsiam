22ab738a83fad86708136aae892344e2a6af9e7e
wanball <wan.ball@gmail.com>
2015-08-07 20:38:25 +0700

    update text editor JS

---
+++ administrator/master/script.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+	if($('.mytextarea').length > 0){
+        tinymce.init({
+            selector: ".mytextarea",
+		    theme: "modern",
+		    plugins: [
+		        "advlist autolink lists link image charmap print preview hr anchor pagebreak",
+		        "searchreplace wordcount visualblocks visualchars code fullscreen",
+		        "insertdatetime media nonbreaking save table contextmenu directionality",
+		        "emoticons template paste textcolor colorpicker textpattern imagetools"
+		    ],
+		    toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent",
+		    toolbar2: "link image | print preview media | forecolor backcolor emoticons",
+		    image_advtab: true
+		});
+	}
+});
