6c550a5279f379e03a2399764c0ecc594ef51466
noppolgap <noppol.vong@gmail.com>
2015-08-07 17:27:52 +0700

    Change to DEV Env.

--- assets/configs/config.inc.php
+++ assets/configs/config.inc.php
@@ -15,7 +15,7 @@
 	define('_DATA_BASE_PASS_','1234');
 	/*------------ Page Config ------------*/
 	define('_TITTLE_SITE_','สถาบันพิพิธฑ์การเรียนรู้แห่งชาติ (สพร.)');
-	define('_FULL_SITE_PATH_','http://localhost:8080/SiamMusuem/museumsiam');
+	define('_FULL_SITE_PATH_','http://localhost:8080/museumsiam');
 	//define('_FULL_SITE_PATH_','http://www.campaignactivity.com/museum');
 	define('_LOG_PATH_','../../log');
 	define('_LANGUAGE_SITE_','thai');
@@ -28,4 +28,3 @@
 	define('_META_KEYWORDS_','มิวเซียม สยาม, สพร., สถาบันพิพิธภัณฑ์การเรียนรู้แห่งชาติ, พิพิธภัณฑ์, พิพิธภัณฑ์ท้องถิ่น, ศูนย์วัฒนธรรม, แหล่งเรียนรู้, ภูมิปัญญา, มรดกวัฒนธรรม, ประวัติศาสตร์ท้องถิ่น, จดหมายเหตุ, พิพิธภัณฑ์วิทยา, Museum Siam, NDMI, National Discovery Museum Institute, Thai Museums , local museum, museum database, thai museums, cultural center, heritage, cultural heritage, intangible cultural heritage, digital archive, museology');
 	define('_META_DESCRIPTION_','มิวเซียมสยาม พิพิธภัณฑ์การเรียนรู้ (Museum Siam: Discovery Museum) ภายใต้ สถาบันพิพิธภัณฑ์การเรียนรู้แห่งชาติ (สพร.) เป็นพิพิธภัณฑ์การเรียนรู้แห่งแรกที่เน้น การสร้างประสบการณ์สดใหม่ในการชมพิพิธภัณฑ์ ซึ่งจัดตั้งขึ้นเพื่อเป็นต้นแบบของ แหล่งเรียนรู้ที่น่ารื่นรมย์ และช่วยยกระดับมาตรฐานการจัดการเรียนรู้ในรูปแบบใหม่');
 ?>
-
