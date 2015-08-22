496911f8b7737860d66485996f54e89e696cca32
Peemai124 <nongmai_kitchakoot@hotmail.com>
2015-08-22 23:23:34 +0700

    side menu

--- administrator/inc_side.php
+++ administrator/inc_side.php
@@ -1,7 +1,13 @@
 <div class="floatL main-menu">
 	<div class="menuhead">เมนูการใช้งาน</div>
-	<? for($i=0;$i<10;$i++){ ?>
-	<a href="#"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">หน้าหลัก</span></a>
-	<? } ?>
+	
+	<a href="#"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">ระบบจัดการผู้ใช้งาน</span></a>
+	<a href="#"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">ระบบจัดการองค์ความรู้</span></a>
+	<a href="../mod_digital_achive/main_digital_view.php"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">คลังข้อมูลดิจิตอล</span></a>
+	<a href="#"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">พิพิธภัณฑ์เครือข่าย</span></a>
+	<a href="../mod_virtual_exhibition/index.php"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">พิพิธภัณฑ์เสมือน</span></a>
+	<a href="#"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">แผนที่พิพิธภัณฑ์</span></a>
+	<a href="../mod_shopping/index.php"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">ระบบ Online</span></a>
+
 	<a href="#"><span class="menutab dBlock signoutTab">ออกจากระบบ</span></a>
 </div>
