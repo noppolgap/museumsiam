d4f29c956c9cd6c4d5fc54c4ac2dcb514a89d27c
wanball <wan.ball@gmail.com>
2015-09-07 18:21:45 +0700

    update front

---
+++ inc/inc-left-content-calendar.php
@@ -0,0 +1,40 @@
+<div class="part-calendar">
+	<div class="box-calendar">
+		<div class="box-top">
+			<div  class="box-top-date">
+				<p class="date">99</p>
+				<div class="line"></div>
+				<p class="month-year">พฤศจิกายน 2559</p>	
+			</div>
+			<a class="btn-arrow left"><</a>
+			<a class="btn-arrow right">></a>
+		</div>
+		<div class="box-bottom">
+			Plugin Calendar
+		</div>
+	</div>
+</div>
+
+<div class="part-conming-event">
+	<div class="box-part-conming-event">
+		<div class="box-list-event">
+			<div class="box-list">
+				<div class="box-date-tumb">
+					<p class="date">99</p>
+					<p class="month">xxx</p>
+				</div>
+				<div class="box-text">
+					<a href=""><p class="text-title TcolorRed">
+						Levitated Mass 340 Ton Giant Stone
+					</p></a>
+					<p class="text-date TcolorGray">
+						28 พ.ย. 2559
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="box-btn">
+			<a  class="btn black">ดูทั้งหมด</a>
+		</div>
+	</div>
+</div>
--- inc/inc-left-content-newsevent.php
+++ inc/inc-left-content-newsevent.php
@@ -1,4 +1,26 @@
 <div class="part-left-title-page">
-	
-	
+	<div class="box-title-page">
+		<img src="images/th/title-news-event.png" alt="title-news-event" width="" height="" />
+	</div>
+</div>
+
+<div class="part-left-search">
+	<div class="box-search">
+		<input type="text" placeholder="ค้นหา">
+	</div>
+</div>
+
+<div class="part-menu-left">
+	<div class="box-menu-left">
+		<ul class="menu-left">
+			<li><a href="">กิจกรรมและข่าวสารของมิวเซียมสยาม</a>
+				<ul class="submenu-left">
+					<li><a href="">กิจกรรมของมิวเซียมสยาม</a></li>
+					<li><a href="">ข่าวประชาสัมพันธ์ของมิวเซียมสยาม</a></li>
+					<li><a href="">ประกาศจัดซื้อจัดจ้าง</a></li>
+				</ul>
+			</li>
+			<li><a href="">กิจกรรมและข่าวสารทั้งหมดของทุกระบบ</a></li>
+		</ul>
+	</div>
 </div>
