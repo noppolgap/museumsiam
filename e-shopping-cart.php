b3d284c2b789ad8b9d7a56f12c389e439f50b3f8
wanball <wan.ball@gmail.com>
2015-09-18 17:49:00 +0700

    update front

---
+++ e-shopping-cart.php
@@ -0,0 +1,165 @@
+<?php
+require("assets/configs/config.inc.php");
+require("assets/configs/connectdb.inc.php");
+require("assets/configs/function.inc.php");
+?>
+<!doctype html>
+<html>
+<head>
+<? require('inc_meta.php'); ?>	
+
+<link rel="stylesheet" type="text/css" href="css/template.css" />
+<link rel="stylesheet" type="text/css" href="css/shopping.css" />
+
+<script>
+	$(document).ready(function(){
+		$(".menutop li.menu6,.menu-left li.menu3").addClass("active");		
+	});
+</script>
+	
+</head>
+
+<body id="cart">
+	
+<?php include('inc/inc-top-bar.php'); ?>
+<?php include('inc/inc-menu.php'); ?>	
+
+<div class="part-nav-main"  id="firstbox">
+	<div class="container">
+		<div class="box-nav">
+			<ol class="cf">
+				<li><a href="index.php"><img src="images/icon-home.png"/></a>&nbsp;&nbsp;&nbsp;>&nbsp;&nbsp;</li>
+				<li><a href="#">ระบบอื่นๆ ที่เกี่ยวข้อง</a>&nbsp;&nbsp;&nbsp;>&nbsp;&nbsp;</li>
+				<li><a href="#">ONLINE SYSTEM</a>&nbsp;&nbsp;&nbsp;>&nbsp;&nbsp;</li>
+				<li class="active">e-SHOPPING</li>
+			</ol>
+		</div>
+	</div>
+</div>
+
+<div class="box-freespace"></div>
+
+<div class="part-main">
+	<div class="container cf">
+		<div class="box-left main-content">
+			<?php include('inc/inc-left-content-shopping.php'); ?>
+		</div>
+		<div class="box-right main-content">
+			<hr class="line-red"/>
+			<div class="box-title-system cf">
+				<h1>e-SHOPPING</h1>
+				<div class="box-btn">
+					<a href="e-shopping.php" class="btn red">ย้อนกลับ</a>
+				</div>
+			</div>		
+				
+			<div class="box-category-main">
+				<div class="box-title cf">
+					<h2>ตะกร้าสินค้า</h2>
+				</div>
+			</div>
+			
+			<div class="box-table-main">
+				<div class="table-row head">
+					<div class="column list">สินค้า</div>
+					<div class="column price">ราคา</div>
+					<div class="column number">จำนวน</div>
+					<div class="column total">มูลค่ารวม</div>
+				</div>
+				<div class="table-row list">
+					<div class="column list cf">
+						<div class="box-left">
+							<div class="box-pic">
+								<img src="http://placehold.it/194x147">
+							</div>
+						</div>
+						<div class="box-right">
+							<p class="text-title">New Look Embroidered Cami Top - Size M</p>
+							<p class="text-id">รหัสสินค้า : <span>999,999</span></p>
+							<p class="text-cate">หมวดหมู่สินค้า : <span>ของที่ระลึก</span></p>
+							<p class="text-detail">
+								Top by New Look<br>
+								- Lightweight wool-mix fabric<br>
+								- Soft-touch finish<br>
+								- All-over check design
+							</p>
+						</div>
+					</div>
+					<div class="column price">999,999</div>
+					<div class="column number"><input type="number" name="number" value="1"></div>
+					<div class="column total">999,999</div>
+					<a href="#" class="btn-delete"><span class="bin"></span>ลบรายการสินค้า</a>
+				</div>
+				<div class="table-row list">
+					<div class="column list cf">
+						<div class="box-left">
+							<div class="box-pic">
+								<img src="http://placehold.it/194x147">
+							</div>
+						</div>
+						<div class="box-right">
+							<p class="text-title">New Look Embroidered Cami Top - Size M</p>
+							<p class="text-id">รหัสสินค้า : <span>999,999</span></p>
+							<p class="text-cate">หมวดหมู่สินค้า : <span>ของที่ระลึก</span></p>
+							<p class="text-detail">
+								Top by New Look<br>
+								- Lightweight wool-mix fabric<br>
+								- Soft-touch finish<br>
+								- All-over check design
+							</p>
+						</div>
+					</div>
+					<div class="column price">999,999</div>
+					<div class="column number"><input type="number" name="number" value="1"></div>
+					<div class="column total">999,999</div>
+					<a href="#" class="btn-delete"><span class="bin"></span>ลบรายการสินค้า</a>
+				</div>
+				<div class="table-row list">
+					<div class="column list cf">
+						<div class="box-left">
+							<div class="box-pic">
+								<img src="http://placehold.it/194x147">
+							</div>
+						</div>
+						<div class="box-right">
+							<p class="text-title">New Look Embroidered Cami Top - Size M</p>
+							<p class="text-id">รหัสสินค้า : <span>999,999</span></p>
+							<p class="text-cate">หมวดหมู่สินค้า : <span>ของที่ระลึก</span></p>
+							<p class="text-detail">
+								Top by New Look<br>
+								- Lightweight wool-mix fabric<br>
+								- Soft-touch finish<br>
+								- All-over check design
+							</p>
+						</div>
+					</div>
+					<div class="column price">999,999</div>
+					<div class="column number"><input type="number" name="number" value="1"></div>
+					<div class="column total">999,999</div>
+					<a href="#" class="btn-delete"><span class="bin"></span>ลบรายการสินค้า</a>
+				</div>
+			</div>
+			
+			<div class="box-total-main cf">
+				<div class="box-left">
+					<div class="box-btn cf">
+						<a href="e-shopping.php" class="btn red">ดูสินค้าเพิ่มเติม</a>
+					</div>
+				</div>
+				<div class="box-right">
+					
+				</div>
+			</div>
+			
+		</div>
+	</div>
+</div>
+
+<div class="box-freespace"></div>
+
+
+
+<?php include('inc/inc-footer.php'); ?>	
+
+</body>
+</html>
