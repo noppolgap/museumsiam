ac340b2b7bb4a3c18a285f5b9c17d692d0cd9d9d
wanball <wan.ball@gmail.com>
2015-11-10 17:02:08 +0700

    update Side Menu

--- administrator/inc_side.php
+++ administrator/inc_side.php
@@ -3,108 +3,101 @@
 		Menu
 	</div>
 
-	<script type="text/javascript">
-		$(document).ready(function() {
-
-			//	$("#accordion").accordion({
-			//		event : "click hoverintent"
-			//	});
-
-		});
-	</script>
-
-	<div id="accordion">
-		<h3>ระบบจัดการผู้ใช้งาน (User Management)</h3>
-		<div>
-			<ul>
-				<li>
+		<div class="bodyBlock">
+			<h3 class="menutab dBlock">User Management</h3>
+			<div class="submenutab">
 					<a href="../mod_user/index.php"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">การจัดการสมาชิก</span></a>
-				</li>
-				<li>
 					<a href="#"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">การจัดการสิทธิ์ผู้ใช้งาน (Permission)</span></a>
-				</li>
-			</ul>
+			</div>
 		</div>
-		<br/>
-		<h3>Management</h3>
-		<div>
-			<a href="../mod_module/index.php"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">การจัดการระบบ</span></a>
-			<a href="../mod_sub_module/index.php"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">การจัดการระบบย่อย</span></a>
-			<a href="../mod_category/index.php"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">การจัดการหมวดหมู่</span></a>
+		<div class="bodyBlock">
+			<h3 class="menutab dBlock">Management</h3>
+			<div class="submenutab">
+				<a href="../mod_module/index.php"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">การจัดการระบบ</span></a>
+				<a href="../mod_sub_module/index.php"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">การจัดการระบบย่อย</span></a>
+				<a href="../mod_category/index.php"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">การจัดการหมวดหมู่</span></a>
+			</div>
 		</div>
-		<br/>
-		<h3>Web Portal </h3>
-		<div>
-			<a href="../mod_hero_banner/hero_banner_view.php"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">การจัดการ Hero Banner</span></a>
-			<a href="../mod_hero_banner/hero_banner_view.php"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">ข้อมูลสำหรับผู้เข้าชม</span></a>
-
-			รู้จักเรา
-			<a href="../mod_content_cms/index.php?gp=VISITOR"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">ความเป็นมาของเรา</span></a>
-			<a href="../mod_organization/index.php"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">โครงสร้างของเรา</span></a>
-			บริการของเรา
-			<a href="../mod_content_cms/index.php?gp=KM"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">ห้องคลังความรู้</span></a>
-			<a href="../mod_content_cms/index.php?gp=ARC"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">ห้องคลังโบราณวัตถุ</span></a>
-			<a href="../mod_content_cms/index.php?gp=KITCHEN"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">ร้านอาหาร</span></a>
-			Muse Shop
-			<a href="../mod_content_cms/index.php?gp=RENT"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">พื้นที่ให้เช่า</span></a>
-			<a href="../mod_content_cms/privilege_edit.php?cmsid=17"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">สิทธิพิเศษ</span></a>
-			กิจกรรมและข่าวสาร
-			<a href="../mod_category/content_view.php?cid=60&MID=12&LV=0&SCID=130"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">กิจกรรม</span></a>
-			<a href="../mod_category/content_view.php?cid=60&MID=12&LV=0&SCID=131"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">ข่าวสาร</span></a>
-			<a href="../mod_category/content_view.php?cid=60&MID=12&LV=0&SCID=132"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">จัดซื้อจัดจ้าง</span></a>
-			การจัดการกิจกรรมและข่าวสาร
-			การอนุมัติกิจกรรมและข่าวสารจากระบบย่อย
-			<a href="../mod_faq/index.php"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">คำถามที่พบบ่อย</span></a>
-
-			ติดต่อเรา
-			Address & Map
-			e-Mail Submit form
-			e-Application
-			เก็บข้อมูลผู้ที่ส่งเรื่องมา
-			Sitemap
-			Social Media
-			<a href="../mod_other_link/index.php"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">ลิงค์อื่นๆ ที่เกี่ยวข้อง</span></a>
-			Muse Mag e-Mail List
-
+		<div class="bodyBlock">
+			<h3 class="menutab dBlock">Header &amp; Footer</h3>
+			<div class="submenutab">
+				<a href="#"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">Sitemap</span></a>
+				<a href="#"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">Social Media</span></a>
+				<a href="../mod_other_link/index.php"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">ลิงค์อื่นๆ ที่เกี่ยวข้อง</span></a>
+				<a href="#"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">Muse Mag e-Mail List</span></a>
+			</div>
 		</div>
-		<br/>
-		<h3>ระบบตรวจสอบและอนุมัติ</h3>
-		<div>
-			Knowledge Management
+		<div class="bodyBlock">
+			<h3 class="menutab dBlock">Web Portal </h3>
+			<div class="submenutab">
+			<a href="../mod_hero_banner/hero_banner_view.php"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">Hero Banner</span></a>
+			<a href="../mod_hero_banner/hero_banner_view.php"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">Visitor</span></a>
+
+			<span class="menutab dBlock" style="background-image: url('../images/small-n-flat/directory.svg');">รู้จักเรา</span>
+			<a href="../mod_content_cms/index.php?gp=VISITOR"><span class="menutab dBlock sub_menutab" style="background-image: url('../images/small-n-flat/house.svg');">ความเป็นมาของเรา</span></a>
+			<a href="../mod_organization/index.php"><span class="menutab dBlock sub_menutab" style="background-image: url('../images/small-n-flat/house.svg');">โครงสร้างของเรา</span></a>
+			<span class="menutab dBlock" style="background-image: url('../images/small-n-flat/directory.svg');">บริการของเรา</span>
+			<a href="../mod_content_cms/index.php?gp=KM"><span class="menutab dBlock sub_menutab" style="background-image: url('../images/small-n-flat/house.svg');">ห้องคลังความรู้</span></a>
+			<a href="../mod_content_cms/index.php?gp=ARC"><span class="menutab dBlock sub_menutab" style="background-image: url('../images/small-n-flat/house.svg');">ห้องคลังโบราณวัตถุ</span></a>
+			<a href="../mod_content_cms/index.php?gp=KITCHEN"><span class="menutab dBlock sub_menutab" style="background-image: url('../images/small-n-flat/house.svg');">ร้านอาหาร</span></a>
+			<span class="menutab dBlock" style="background-image: url('../images/small-n-flat/directory.svg');">Muse Shop</span>
+			<a href="../mod_content_cms/index.php?gp=RENT"><span class="menutab dBlock sub_menutab" style="background-image: url('../images/small-n-flat/house.svg');">พื้นที่ให้เช่า</span></a>
+			<a href="../mod_content_cms/privilege_edit.php?cmsid=17"><span class="menutab dBlock sub_menutab" style="background-image: url('../images/small-n-flat/house.svg');">สิทธิพิเศษ</span></a>
+			<span class="menutab dBlock" style="background-image: url('../images/small-n-flat/directory.svg');">กิจกรรมและข่าวสาร</span>
+			<a href="../mod_category/content_view.php?cid=60&MID=12&LV=0&SCID=130"><span class="menutab dBlock sub_menutab" style="background-image: url('../images/small-n-flat/house.svg');">กิจกรรม</span></a>
+			<a href="../mod_category/content_view.php?cid=60&MID=12&LV=0&SCID=131"><span class="menutab dBlock sub_menutab" style="background-image: url('../images/small-n-flat/house.svg');">ข่าวสาร</span></a>
+			<a href="../mod_category/content_view.php?cid=60&MID=12&LV=0&SCID=132"><span class="menutab dBlock sub_menutab" style="background-image: url('../images/small-n-flat/house.svg');">จัดซื้อจัดจ้าง</span></a>
+			<span class="menutab dBlock" style="background-image: url('../images/small-n-flat/directory.svg');">การจัดการกิจกรรมและข่าวสาร</span>
+			<span class="menutab dBlock" style="background-image: url('../images/small-n-flat/directory.svg');">การอนุมัติกิจกรรมและข่าวสารจากระบบย่อย</span>
+			<a href="../mod_faq/index.php"><span class="menutab dBlock sub_menutab" style="background-image: url('../images/small-n-flat/house.svg');">คำถามที่พบบ่อย</span></a>
+
+			<a href="#"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">ติดต่อเรา</span></a>
+			<a href="#"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">Address & Map</span></a>
+			<a href="#"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">e-Mail Submit form</span></a>
+			<a href="#"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">e-Application</span></a>
+			<a href="#"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">เก็บข้อมูลผู้ที่ส่งเรื่องมา</span></a>
+
+			</div>
+		</div>
+		<div class="bodyBlock">
+		<h3 class="menutab dBlock">ระบบตรวจสอบและอนุมัติ</h3>
+			<div class="submenutab">
+
+			<a href="#"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">Knowledge Management
 			<a href="../mod_webboard/index.php"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">การจัดการ เว็บบอร์ด</span></a>
+			<a href="#"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">อนุมัติการลบกระทู้</span></a>
 
-			อนุมัติการลบกระทู้
-
-			Museum Data Network
+			<a href="#"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">Museum Data Network</span></a>
 			<a href="../mod_network_museum/pending_museum_view.php"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">การจัดการพิพิธภัณฑ์</span></a>
 
-			การจัดการกิจกรรมและข่าวสาร
-			กิจกรรมและข่าวสาร
-			กิจกรรม
-			ข่าวสาร
+			<a href="#"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">การจัดการกิจกรรมและข่าวสาร</span></a>
+			<a href="#"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">กิจกรรมและข่าวสาร</span></a>
+			<a href="#"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">กิจกรรม</span></a>
+			<a href="#"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">ข่าวสาร</span></a>
 
+			</div>
 		</div>
-		<br/>
-		<h3>e-Booking</h3>
-		<div>
-			การจัดการ booking
-			การอนุมัติ
+		<div class="bodyBlock">
+		<h3 class="menutab dBlock">e-Booking</h3>
+			<div class="submenutab">
+
+			<a href="#"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">การจัดการ booking</span></a>
+			<a href="#"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">การอนุมัติ</span></a>
+			</div>
 		</div>
-		<br/>
-		<h3>e-Shopping</h3>
+		<h3 class="menutab dBlock">e-Shopping</h3>
 		<div>
+			<div class="submenutab">
 
 			<a href="../mod_order/index.php"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">การจัดการสินค้า</span></a>
-			อัพเดทการขนส่ง
+			<a href="#"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">อัพเดทการขนส่ง</span></a>
+			</div>
 		</div>
 
-		<br/>
-		<h3>การออก Report</h3>
+		<h3 class="menutab dBlock">การออก Report</h3>
 		<div>
 
 		</div>
-
-	</div>
 
 	<!--<a href="../mod_sub_category/index.php"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">การจัดการหมวดหมู่ย่อย</span></a>
 	<a href="../mod_content/index.php"><span class="menutab dBlock" style="background-image: url('../images/small-n-flat/house.svg');">การจัดการเนื้อหา</span></a>-->
